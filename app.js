8d890d2eb303ecb8235054b64a06e2d3981d0345
thami memel <memelthami@gmail.com>
2019-07-21 01:33:25 +0100

    change for some variables from let to const

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-let productContainer = document.querySelector(".products-container");
-let cartBtn = document.querySelector(".cart-btn");
-let cartCloseBtn = document.querySelector(".close-cart");
-let cartDOM = document.querySelector(".cart");
-let overlay = document.querySelector(".overlay");
-let cartContainer = document.querySelector(".cart-container");
+const productContainer = document.querySelector(".products-container");
+const cartBtn = document.querySelector(".cart-btn");
+const cartCloseBtn = document.querySelector(".close-cart");
+const cartDOM = document.querySelector(".cart");
+const overlay = document.querySelector(".overlay");
+const cartContainer = document.querySelector(".cart-container");
 
 let addToCartBtns = [];
 let products = [];
@@ -22,53 +22,54 @@ cartCloseBtn.addEventListener("click", () =>{
 // Getting data from JSON file 
 async function getProducts(url) {
   try {
-    let result = await fetch(url);
-    let data = await result.json();
+    const result = await fetch(url);
+    const data = await result.json();
     return data;
   } catch (err) {
     console.log(err);
   }
 }
 
-// rendering products 
+// Rendering products 
 function renderProducts(arr) {
   let result = "";
   arr.forEach(product => {
+    const {image, name, price,id} = product;
     result +=
       `
-        <article class="product" data-id="${product.id}">
+        <article class="product" data-id="${id}">
           <div class="product-image">
-            <img src="${product.image}">
+            <img src="${image}">
           </div>
           <div class="product-info">
-            <h5 class="product-name">${product.name}</h5>
-            <h6 class="product-price">$${product.price.toFixed(2)}</h6>
+            <h5 class="product-name">${name}</h5>
+            <h6 class="product-price">$${price.toFixed(2)}</h6>
           </div>
-          <button class="add-item" data-id="${product.id}"><i class="material-icons"> add_shopping_cart </i> Add to cart</button>
+          <button class="add-item" data-id="${id}"><i class="material-icons"> add_shopping_cart </i> Add to cart</button>
         </article>
         `;
   });
   productContainer.innerHTML = result;
 }
 
-// Render Cart Items
-
+// Rendering Cart Items
 function renderCartItem(obj) {
   let result = "";
+  const {image, name, price,id} = obj;
   result += `
       <div class="cart-item">
       <div class="thumbnail">
-        <img src="${obj.image}" />
+        <img src="${image}" />
       </div>
       <div class="cart-item-infos">
-        <h4 class="cart-item-title">${obj.name}</h4>
-        <p class="cart-item-price">$${obj.price.toFixed(2)}</p>
-        <a href="#" class="remove" data-id="${obj.id}">remove</a>
+        <h4 class="cart-item-title">${name}</h4>
+        <p class="cart-item-price">$${price.toFixed(2)}</p>
+        <a href="#" class="remove" data-id="${id}">remove</a>
       </div>
       <div class="cart-item-counter">
-        <i class="material-icons increase" data-id="${obj.id}"> keyboard_arrow_up </i>
+        <i class="material-icons increase" data-id="${id}"> keyboard_arrow_up </i>
         <span class="count">1</span>
-        <i class="material-icons decrease" data-id="${obj.id}"> keyboard_arrow_down </i>
+        <i class="material-icons decrease" data-id="${id}"> keyboard_arrow_down </i>
       </div>
     </div>
     `;
@@ -78,7 +79,7 @@ function renderCartItem(obj) {
 // Manage add to cart Buttons
 function addToCartBtnsManager(arr) {
   arr.forEach(btn => {
-    let id = parseInt(btn.getAttribute("data-id"));
+    const id = parseInt(btn.getAttribute("data-id"));
     // Disabling buttons for items already in cart
     if (cart.find(e => e.id === id)) {
       btn.innerHTML = "In Cart";
@@ -87,7 +88,7 @@ function addToCartBtnsManager(arr) {
     // Adding products to cart onclick and disabling button
     btn.addEventListener("click", () => {
       if (cart.find(e => e.id === id) === undefined) {
-        let currentProduct = products.find(e => e.id === id);
+        const currentProduct = products.find(e => e.id === id);
         cart.push(currentProduct);
         btn.innerHTML = "In Cart";
         btn.setAttribute("disabled", "true");
@@ -98,8 +99,12 @@ function addToCartBtnsManager(arr) {
   })
 }
 getProducts("products.json").then(data => {
+  // Getting products from JSON file
   products = data.products;
+  // Rendering the products to the DOM
   renderProducts(products);
+  // Adding "add to cart Buttons" to the a vairable
   addToCartBtns = document.querySelectorAll(".add-item");
+  // Setting up each button with an event listener
   addToCartBtnsManager(addToCartBtns);
 });
